4b31cbb062fd0f353f51536c5b3ad6a04a6ad5e2
CronoPT <vasco.dias.silva@tecnico.ulisboa.pt>
2019-12-01 17:57:31 +0000

    Add function to compute center point of cells

--- src/map_discretizer.cpp
+++ src/map_discretizer.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <utility>
 
 #include "ros/ros.h"
 #include "ros/console.h"
@@ -65,6 +67,8 @@ class map_discretizer {
 		return (x>=_init_x && x<=_final_x && y>=_init_y && y<=_final_y);
 	}
 
+	
+
 	public:
 	map_discretizer(nav_msgs::OccupancyGrid map, double cell_size):
 		_map(map), _init_x(-1), _init_y(-1),
@@ -73,6 +77,61 @@ class map_discretizer {
 		compute_init_y();
 		compute_final_x();
 		compute_final_y();
+	}
+
+	void print_info() {
+		ROS_INFO("Pose of origin -> (%.3f, %.3f)", _map.info.origin.position.x, _map.info.origin.position.y);
+	}
+
+	void print_position_of_pixel(int x, int y) {
+		double world_x = x*_map.info.resolution - _map.info.origin.position.x;
+		double world_y = y*_map.info.resolution - _map.info.origin.position.y;
+		ROS_INFO("Pixel (%d, %d) corresponds to position (%.3f, %.3f) in the map", x, y, world_x, world_y);
+	}
+
+	double pixel_x_to_coordinate(int x) {
+		return x*_map.info.resolution - _map.info.origin.position.x;
+	}
+
+	double pixel_y_to_coordinate(int y) {
+		return y*_map.info.resolution - _map.info.origin.position.y;
+	}
+
+	int discretized_grid_size_x() {
+		return (_final_x - _init_x)*_map.info.resolution/_cell_size + 1;
+	}
+
+	int discretized_grid_size_y() {
+		return (_final_y - _init_y)*_map.info.resolution/_cell_size + 1;
+	}
+
+	void compute_network() {
+		std::vector<std::pair<int, int>> states;
+
+		double init_square_x = 0;
+		double init_square_y = 0;
+		double final_square_x = 0;
+		double final_square_y = 0;
+		double coord_x = 0;
+		double coord_y = 0;
+		int pixels_per_square = _cell_size/_map.info.resolution + 1;
+		int i = 0;
+
+
+		for(int y=discretized_grid_size_y()-1; y>=0; y--) {
+			for(int x=0; x<discretized_grid_size_x(); x++, i++) {
+				init_square_x = pixel_x_to_coordinate( (_init_x+x)*pixels_per_square );
+				init_square_y = pixel_y_to_coordinate( (_init_y+y)*pixels_per_square );
+				final_square_x = pixel_x_to_coordinate( (_init_x+x+1)*pixels_per_square );
+				final_square_y = pixel_y_to_coordinate( (_init_y+y+1)*pixels_per_square );
+				coord_x = (init_square_x+final_square_x)/2;
+				coord_y = (init_square_y+final_square_y)/2;
+				std::cout << "|" << std::setprecision(2) << std::fixed;
+				std::cout << coord_x << " " << coord_y; 
+			}
+			std::cout << std::endl;
+		}
+
 	}
 
 	void paint_map(const std::string& path) {
@@ -154,8 +213,7 @@ int main(int argc, char* argv[]) {
 	nav_msgs::GetMap::Request  req;
 	nav_msgs::GetMap::Response resp;
 	ROS_INFO("Requesting the map...");
-	while(!ros::service::call("static_map", req, resp))
-	{
+	while(!ros::service::call("static_map", req, resp)) {
 		ROS_WARN("Request for map failed; trying again...");
 		ros::Duration d(0.5);
 		d.sleep();
@@ -165,6 +223,11 @@ int main(int argc, char* argv[]) {
 	
 	map_discretizer m_d(resp.map, 0.3);
 	m_d.paint_chess_map("mapina.ppm");
+	m_d.print_info();
+	m_d.print_position_of_pixel(100, 100);
+	m_d.compute_network();
+	ROS_INFO("Result grid size <%d x %d>", m_d.discretized_grid_size_x(), m_d.discretized_grid_size_y());
+
 
 	ROS_INFO("Finished");
 
